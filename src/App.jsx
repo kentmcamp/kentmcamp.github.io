2a09083a1108f2b0cee29f478d737c66b01cda1e
Kent Campbell <kentmcamp87@gmail.com>
2025-01-14 19:25:05 -0400

    added custom icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,53 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Under Construction</h1>
-      <span className="material-icons" style={{ fontSize: 100, color: '#4AE4FF' }}>build</span>
-      <p>Welcome to my portfolio. This site is currently under construction. Please check back soon for updates.</p>
+      <span
+        className="material-icons"
+        style={{ fontSize: 100, color: "#4AE4FF" }}
+      >
+        build
+      </span>
+      <p>
+        Welcome to my portfolio.
+        <br />
+        This site is currently under construction.
+        <br />
+        Please check back soon for updates.
+      </p>
       <button>
-        <a href="https://kentmcamp.github.io/webAudio/" target="_blank" rel="noreferrer">Play A Song</a>
+        <a
+          href="https://kentmcamp.github.io/webAudio/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Play A Song
+        </a>
       </button>
       <div className="contact-info">
+
+        <a href="https://github.com/kentmcamp" className="icon-link">
+          <button className="small-button">
+            <img src="./public/assets/icons/github.webp" alt="GitHub" />
+          </button>
+        </a>
+
         <a href="mailto:kentmcamp87@gmail.com" className="icon-link">
-          <span className="material-icons">email</span>
+          <button className="small-button">
+            <img src="./public/assets/icons/email.webp" alt="Email" />
+          </button>
         </a>
-        <a href="https://github.com/kentmcamp" target="_blank" rel="noreferrer" className="icon-link">
-          <span className="material-icons">code</span>
-        </a>
-        <a href="https://www.linkedin.com/in/kentmcampbell" target="_blank" rel="noreferrer" className="icon-link">
-          <span className="material-icons">business_center</span>
+
+        <a href="https://www.linkedin.com/in/kentmcampbell" className="icon-link">
+          <button className="small-button">
+            <img src="./public/assets/icons/linkedIn.webp" alt="LinkedIn" />
+          </button>
         </a>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
